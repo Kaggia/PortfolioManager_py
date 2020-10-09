adfe158777a8af4d7ff793f7297747b2f85c0364
Kaggia <zimmittistefano@hotmail.it>
2020-10-09 14:41:42 +0200

    Updated GUI in subwindow temporal analysis

--- src/subwindows_report_tab/subwindow_temporal_analysis.py
+++ src/subwindows_report_tab/subwindow_temporal_analysis.py
@@ -40,15 +40,88 @@ class TemporalAnalysisWindow:
 
         #Button_Previous
         self.prev_btn = QtWidgets.QPushButton(self.frame)
-        self.prev_btn.setGeometry(QtCore.QRect((self.frame.size().width() / 2) - 100, int(self.frame.size().height() *0.6), 100, 40))
+        self.prev_btn.setGeometry(QtCore.QRect((self.frame.size().width() / 2) - 100, int(self.frame.size().height() *0.70), 100, 40))
         self.prev_btn.setText("<= Previous page")
         self.prev_btn.setEnabled(True)
         #Button_Previous
         self.next_btn = QtWidgets.QPushButton(self.frame)
-        self.next_btn.setGeometry(QtCore.QRect((self.frame.size().width() / 2), int(self.frame.size().height() *0.6), 100, 40))
+        self.next_btn.setGeometry(QtCore.QRect((self.frame.size().width() / 2), int(self.frame.size().height() *0.70), 100, 40))
         self.next_btn.setText("Next page =>")
         self.next_btn.setEnabled(True)
 
+        self.__load_groupbox_temp_selection__()
+        self.__load_groupbox_year_selection__()
+        self.__load_groupbox_month_selection__()
+
+
+    def __load_groupbox_temp_selection__(self):
+        groupbox_temp_choice = QtWidgets.QGroupBox("Choose temporal window", self.frame )
+        groupbox_temp_choice.setGeometry(QtCore.QRect(50, 450, 175, 100))
+        
+        vbox = QtWidgets.QVBoxLayout()
+        groupbox_temp_choice.setLayout(vbox)
+
+        #RadioButton
+        self.monthly_choice_rb = QtWidgets.QRadioButton()
+        self.monthly_choice_rb.setGeometry(QtCore.QRect(100, 450, 100, 40))
+        self.monthly_choice_rb.setText("Monthly")
+        self.monthly_choice_rb.setEnabled(True)
+        self.monthly_choice_rb.setChecked(False)
+
+        #RadioButton
+        self.yearly_choice_rb = QtWidgets.QRadioButton()
+        self.yearly_choice_rb.setGeometry(QtCore.QRect(100, 475, 100, 40))
+        self.yearly_choice_rb.setText("Yearly")
+        self.yearly_choice_rb.setEnabled(True)
+        self.yearly_choice_rb.setChecked(True)
+
+        vbox.addWidget(self.monthly_choice_rb)
+        vbox.addWidget(self.yearly_choice_rb)
+
+    def __load_groupbox_year_selection__(self):
+        groupbox_year_choice = QtWidgets.QGroupBox("Choose year to show", self.frame )
+        groupbox_year_choice.setGeometry(QtCore.QRect(250, 450, 175, 100))
+        
+        vbox = QtWidgets.QVBoxLayout()
+        groupbox_year_choice.setLayout(vbox)
+
+        #Combobox
+        self.combobox_year_select = QtWidgets.QComboBox()
+        self.combobox_year_select.setGeometry(QtCore.QRect(300, 450 , 150, 25))
+        self.combobox_year_select.addItem('2000')
+        self.combobox_year_select.addItem('2001')
+        self.combobox_year_select.addItem('2002')
+
+        self.combobox_year_select.setCurrentIndex(0)
+
+        vbox.addWidget(self.combobox_year_select)
+
+    def __load_groupbox_month_selection__(self):
+        groupbox_month_choice = QtWidgets.QGroupBox("Choose month to show", self.frame )
+        groupbox_month_choice.setGeometry(QtCore.QRect(450, 450, 175, 100))
+        
+        vbox = QtWidgets.QVBoxLayout()
+        groupbox_month_choice.setLayout(vbox)
+
+        #Combobox
+        self.combobox_month_select = QtWidgets.QComboBox()
+        self.combobox_month_select.setGeometry(QtCore.QRect(500, 450 , 150, 25))
+        self.combobox_month_select.addItem('January')
+        self.combobox_month_select.addItem('February')
+        self.combobox_month_select.addItem('March')
+        self.combobox_month_select.addItem('April')
+        self.combobox_month_select.addItem('May')
+        self.combobox_month_select.addItem('June')
+        self.combobox_month_select.addItem('July')
+        self.combobox_month_select.addItem('August')
+        self.combobox_month_select.addItem('September')
+        self.combobox_month_select.addItem('October')
+        self.combobox_month_select.addItem('November')
+        self.combobox_month_select.addItem('December')
+
+        self.combobox_month_select.setCurrentIndex(0)
+
+        vbox.addWidget(self.combobox_month_select)
     #Detect temporal view: YEARS - MONTHS - DAYS
     def __detect_initial_temporal_view__(self):
         temporal_view = 'd'
@@ -59,9 +132,7 @@ class TemporalAnalysisWindow:
         pass
     #Load data on chart
     def load_data_on_chart(self):
-        if self.current_temporal_view == 'd':
-            pass
-        elif self.current_temporal_view == 'm':
+        if self.current_temporal_view == 'm':
             pass
         elif self.current_temporal_view == 'y':
             pass
